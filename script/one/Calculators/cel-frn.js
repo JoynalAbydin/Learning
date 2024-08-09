b19f9e2982ae75007878aeef6bb49434be8d5070
JoynalAbydin <157563877+JoynalAbydin@users.noreply.github.com>
2024-08-09 23:04:50 +0600

    Delete script directory

--- script/one/Calculators/cel-frn.js
+++
@@ -1,3 +0,0 @@
-var cels = parseFloat(prompt("Enter Celsius : "));
-var frns = (cels*9/5)+32;
-document.write("আপনি ইনপুট করেছেন "+cels+"° Celsius যা Fahrenheit এ কনভার্ট করলে হবে = "+frns+"° Fahrenheit");
--- script/one/other/head.js
+++
@@ -1,7 +0,0 @@
-function loadHeading() {
-  const headingElement = document.createElement("header");
-  document.body.appendChild(headingElement);
-}
-
-// Call this function on page load or when needed
-loadHeading();
